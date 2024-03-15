2b7431e7e689bc9269fac31f9ab39ff0a3c4fd5f
jagadisholi <olijagadish1@gamil.com>
2024-03-15 08:20:04 +0545

    form

---
+++ form.js
@@ -0,0 +1,4 @@
+function copyTemporaryToPermanent() {
+    var temporaryAddress = document.getElementById('temporaryAddress').value;
+    document.getElementById('permanentAddress').value = temporaryAddress;
+}
