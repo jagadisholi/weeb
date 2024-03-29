3aba5c4333601cc88567ff308a52d45019d7c073
jagadisholi <olijagadish1@gamil.com>
2024-03-29 21:22:21 +0545

    header

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 // Function to update the current time
 function updateCurrentTime() {
-    const timeElement = document.getElementById("currentTime");
-    const now = new Date();
-    timeElement.innerHTML = now.toLocaleTimeString();
-  }
-  
-  // Function to update the current date
-  function updateCurrentDate() {
-    const dateElement = document.getElementById("currentDate");
-    const now = new Date();
-    const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const dayOfWeek = day[now.getDay()];
-    const formattedDate = ${dayOfWeek}, ${month[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()};
-    dateElement.innerHTML = formattedDate;
-  }
-  
-  // Initial calls to update the date and time when the page loads
-  updateCurrentDate();
-  updateCurrentTime();
-  
-  // Update the date and time every second
-  setInterval(updateCurrentDate, 1000); // Updates date every second
-  setInterval(updateCurrentTime, 1000); // Updates time every second
-  
+  const timeElement = document.getElementById("currentTime");
+  const now = new Date();
+  timeElement.innerHTML = now.toLocaleTimeString();
+}
+
+// Function to update the current date
+function updateCurrentDate() {
+  const dateElement = document.getElementById("currentDate");
+  const now = new Date();
+  const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const dayOfWeek = day[now.getDay()];
+  const formattedDate = `${dayOfWeek}, ${month[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()} 
+`;     dateElement.innerHTML = formattedDate;
+ }
+
+//  Initial calls to update the date and time when the page loads
+ updateCurrentDate();
+ updateCurrentTime();
+
+// Update the date and time every second
+setInterval(updateCurrentDate, 1000); // Updates date every second
+setInterval(updateCurrentTime, 1000); // Updates time every second
